9e426278bc0318a1778a4ea9856729bfa3d61cda
Jamie Zhang <jamie.zhang@outlook.com>
2019-05-06 09:16:47 +1200

    wip

--- model.py
+++ model.py
@@ -23,11 +23,33 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        self.embed_size = embed_size
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        
+        
+        
+        self.word_emb = nn.Linear(vocab_size, embed_size)
+        
+        self.lstm = nn.LSTM(self.embed_size, self.hidden_size, self.num_layers, 
+                            dropout= 0.5 , batch_first=True)
+        
+        # self.dropout = nn.Dropout(drop_prob)
+        
+        self.fc = nn.Linear(n_hidden, vocab_size)
     
     def forward(self, features, captions):
-        pass
-
+        # input entire sequence as suggested in the project introduction 
+        embeddeds = [self.word_emb(caption) for caption in captions]
+        inputs = torch.cat([features], embeddeds)
+        
+        hidden = (torch.randn(1, 1, 3), torch.randn(1, 1, 3))  # clean out hidden state
+        
+        out, hidden = lstm(inputs, hidden)
+        
+        return out
+       
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
         pass
